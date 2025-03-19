6ad4a58674279d9836d0d762067923621d46a153
Os-devAi <omartinezd3@miumg.edu.gt>
2025-03-18 22:57:28 -0600

    V 1.0.0 feat: add responsive iframe for location section
    Added a new CSS file `iFrame.css` to style the iframe container and its parent for the location section in `App.jsx`. The iframe now displays a Google Maps embed with responsive design and rounded corners. This enhances the user experience by providing a visually appealing and functional map display.

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import "./App.css";
 import "./Form.css";
+import "./iFrame.css";
 import phoneMaintenance from "./assets/phone_maintenance_1.png";
 import apple from "./assets/apple.png";
 import samsung from "./assets/samsung.png";
@@ -254,9 +255,31 @@ function App() {
       </section>
 
       {/* seccion acerca de */}
-      <section id="productos">
+      {/*<section id="productos">
         <h2 color="#FFFfff">Sobre Servicel Salamá</h2>
-        <div className="products-container"></div>
+        <div className="products-container">
+          <p>
+            
+          </p>
+        </div>
+      </section> */}
+
+      {/* seccion ubicación */}
+      <section id="ubicacion">
+        <h2 color="#FFFfff">Ubicación</h2>
+        <div className="parent-container">
+          <div className="iframe-container">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d425.7642485640927!2d-90.3223661517491!3d15.104128195108933!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1ses-419!2sgt!4v1742341904409!5m2!1ses-419!2sgt"
+              width="600"
+              height="450"
+              style={{ border: "0" }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            />
+          </div>
+        </div>
       </section>
 
       {/* seccion contacto */}
